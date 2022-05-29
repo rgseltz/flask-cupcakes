2ee7b6c775a5892b590704b66f9d40af1d8a8fdc
ryan <ryanjgecelter@gmail.com>
2022-05-29 11:24:27 +0300

    added a DELETE request to route '/api/cupcakes/<id>' to delete a cupcake

--- app.py
+++ app.py
@@ -46,3 +46,11 @@ def update_a_cupcake(id):
     db.session.add(cupcake)
     db.session.commit()
     return jsonify(data=cupcake.serialized())
+
+
+@app.route('/api/cupcakes/<int:id>', methods=["DELETE"])
+def delete_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message="Deleted")
