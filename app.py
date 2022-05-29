5a365791c226ee8cc77c5fe6ebaf78200fba37ae
ryan <ryanjgecelter@gmail.com>
2022-05-29 10:21:18 +0300

    created Cupcake class that instatiates new cupcakes to cupcakes table in cupcakes_db

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template, session, flash
-from models import db, connect_db
+from models import db, connect_db, Cupcake
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-
--- models.py
+++ models.py
@@ -1,7 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
     print('connected.')
+
+
+class Cupcake(db.Model):
+    """Cupcake class that instatiates cupcake objects"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+    flavor = db.Column(db.Text, nullable=False)
+
+    size = db.Column(db.Text, nullable=False)
+
+    rating = db.Column(db.Float, nullable=False)
+
+    image = db.Column(db.Text, nullable=False,
+                      default='https://tinyurl.com/demo-cupcake')
+
+    def __repr__(self):
+        return f"<Cupcake id={self.id}, flavor={self.flavor}, size={self.size}, rating={self.rating}, image={self.image}>"
