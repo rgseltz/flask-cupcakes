1fc5c6d3b2912e94f4a680f77d1c178e106edd80
ryan <ryanjgecelter@gmail.com>
2022-05-29 10:07:29 +0300

    connected database to flask app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, request, redirect, render_template, session, flash
+from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_db'
+
+connect_db(app)
+
